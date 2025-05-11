474459e8dcbedb8349c3ecb536b8712cc8e1ea72
MrLuigi-prjct <luigi.guernelli.corso@gmail.com>
2025-05-11 21:52:19 +0200

    Create NomiOrdineInverso.java
    Esercizio: ArrayList: nomi in ordine inverso

---
+++ back-end/array-e-liste/NomiOrdineInverso.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NomiOrdineInverso {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> listaNomi = new ArrayList<>();
+        
+        // Chiedi 3 nomi all'utente
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Inserisci il nome " + (i + 1) + ": ");
+            String nome = scanner.nextLine();
+            listaNomi.add(nome);
+        }
+        
+        // Stampa i nomi in ordine inverso
+        System.out.println("Nomi in ordine inverso:");
+        for (int i = listaNomi.size() - 1; i >= 0; i--) {
+            System.out.println(listaNomi.get(i));
+        }
+        
+        scanner.close();
+    }
+}
