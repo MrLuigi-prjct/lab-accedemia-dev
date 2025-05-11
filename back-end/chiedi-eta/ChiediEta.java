ec11d2b7995356193ebab6eadb8b44470e94d6ac
MrLuigi-prjct <luigi.guernelli.corso@gmail.com>
2025-05-11 20:46:44 +0200

    Create ChiediEta.java

---
+++ back-end/chiedi-eta/ChiediEta.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ChiediEta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quanti anni hai? ");
+        String nome = scanner.nextLine();
+        System.out.println("Benvenuto, " + nome + "!");
+    }
+}
